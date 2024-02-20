59d826fc90cf16640a2be0791956b8eb0b204112
Frank Adams <fma7308@gmail.com>
2024-02-19 22:54:26 -0500

    Added configurations ettings

--- combined.php
+++ combined.php
@@ -234,11 +234,14 @@
     <script>
         document.addEventListener('DOMContentLoaded', function () {
             const thumbnails = document.querySelectorAll('.thumbnail');
+            const popupWidth = 1280;
+            const popupHeight = 720;
+
             thumbnails.forEach(thumbnail => {
                 thumbnail.addEventListener('click', function () {
                     const imagePath = this.src;
                     const imageName = this.alt;
-                    window.open(imagePath, "", "width=1024, height=768");   // Display full-size image in a new window
+                    window.open(imagePath, imageName, `width=${popupWidth}, height=${popupHeight}`);
                 });
             });
         });
