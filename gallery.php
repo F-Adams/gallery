83aa7bd898f1cc20b7de031fcb8e68fda99cbfa6
Frank Adams <fma7308@gmail.com>
2024-03-01 02:16:37 -0500

    Variable changes for testing

--- gallery.php
+++ gallery.php
@@ -15,7 +15,7 @@
     <?php
         // Configurable things
         $extensions = "jpg,jpeg,png,gif";       // Supported image file extensions
-        $thumbsPerPage = 25;                    // Number of thumbnails to display per page
+        $thumbsPerPage = 50;                    // Number of thumbnails to display per page
         $maxLinks = 10;                         // Number of pagination links to show
         $imageFolder = "images";                // Folder containing full sized image files
         $thumbFolder = "thumbs";                // Folder containing scaled thumbnail images
